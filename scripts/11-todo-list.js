dbe66c33a2c2dc7a19d203f2d2f4ad56f1239841
Manyan06 <manya06sharada@gmail.com>
2025-03-27 22:41:10 +0530

    Ch11-part2 updated

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -5,7 +5,13 @@
 // 4. Add it to Array
 // 5. console.log() the array
 
-const todoList =  ['add', 'sub'];
+const todoList =  [{
+  name :'add',
+  dueDate: '2022-12-22'
+}, {
+  name:'sub',
+  dueDate: '2022-12-23'
+}];
 
 renderTodoList();
 
@@ -14,8 +20,28 @@ function renderTodoList() {
 let todoListHTML = '';
 
 for (let i = 0;  i< todoList.length; i++) {
-  const todo = todoList[i];
-  const html = `<p>${todo}</p>`;
+  const todoObject = todoList[i];
+
+  // const name = todoObject.name;
+  // const dueDate = todoObject.dueDate;
+
+  //Destructuring (shortcut)
+  const {name, dueDate} = todoObject;
+
+  const html = `
+  <p>
+    ${name} ${dueDate}
+     <button onclick="
+     todoList.splice(${i}, 1); 
+     
+     //splice takes in 2 numbers
+     // 1.The index we want to remove
+     // 2.How many values to remove
+
+     renderTodoList();
+     "> Delete </button>
+  </p>
+  `;
   todoListHTML += html;
 }
 console.log(todoListHTML);
@@ -26,9 +52,11 @@ document.querySelector('.js-todo-list')
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
-  const name = inputElement.value
+  const name = inputElement.value;
   
-
+  const dateInputElement = document.querySelector('.js-due-date-input');
+   const dueDate = dateInputElement.value;
+   
   todoList.push(name);  // make it array
   console.log(todoList);
 
